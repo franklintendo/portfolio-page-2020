f411089718b729a9117af9bbaacfe07fc3304875
Frank Moran <info@francisfrankmoran.com>
2020-03-31 13:55:57 -0400

    employee summary and cinefile in projects

--- src/components/Home/Project.js
+++ src/components/Home/Project.js
@@ -8,15 +8,15 @@ const renderProjects = (items) => {
                 <div className="projects--img-container">
                 <img src={item.image} alt="Mad Libs" className="w-100"/>
                 </div>
-                <p className="projects--title mb-0 px-3 pt-3">{item.title}</p>
+                <p className="projects--title mb-0 px-3 pt-3 pb-1">{item.title}</p>
                 <p className="projects--deets mb-3 pb-3 px-3">{item.deets.text}
                     <br/>
                 <small>{item.deets.small}</small>
                 </p>
                 
-                <a className="projects--link px-3 py-1" href={item.demo} rel="noopener noreferrer" target="_blank"><i class="fas fa-laptop-code"></i>&nbsp;&nbsp;Demo</a>
+                <a className="projects--link px-3 py-2" href={item.demo} rel="noopener noreferrer" target="_blank"><i class="fas fa-laptop-code"></i>&nbsp;&nbsp;Demo</a>
                 &nbsp;&nbsp;&nbsp;
-                <a className="projects--link px-3 py-1" href={item.github} rel="noopener noreferrer" target="_blank"><i class="fab fa-github"></i>&nbsp;&nbsp;Github</a>
+                <a className="projects--link px-3 py-2" href={item.github} rel="noopener noreferrer" target="_blank"><i class="fab fa-github"></i>&nbsp;&nbsp;Github</a>
 
                 </div>
         </Col>
--- src/components/Home/Projects.js
+++ src/components/Home/Projects.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Project from './Project';
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import projects from "../../projects.json";
 
 class Projects extends Component {
@@ -15,28 +15,6 @@ class Projects extends Component {
             <Container fluid className="py-5 projects--container" style={{}}>
                 <Row>
                     <Project projects={this.state.projects} />
-                  
-                    
-                    <Col md={4} lg={3}>
-                          <p>Employee Summary Generator</p>
-                          <p>A Node command-line interface that gathers employee information and generates a team page in HTML.
-                                <br/>
-                          <small>Includes: Node.js, Inquirer</small>
-                          </p>
-                          <a href="https://youtu.be/t-sPqBDT3xw" rel="noopener noreferrer" target="_blank">Demo</a>
-                          &nbsp;
-                          <a href="https://github.com/franklintendo/employee-summary" rel="noopener noreferrer" target="_blank">Github</a>
-                    </Col>
-                    <Col md={4} lg={3}>
-                          <p>Developer Profile Generator</p>
-                          <p>A command-line application that dynamically generates a PDF profile from a GitHub username.
-                                <br/>
-                          <small>Includes: Node.js, Inquirer, Axios, HTML5ToPDF</small>
-                          </p>
-                          <a href="https://youtu.be/i7mSYUejJyc" rel="noopener noreferrer" target="_blank">Demo</a>
-                          &nbsp;
-                          <a href="https://github.com/franklintendo/profile-generator" rel="noopener noreferrer" target="_blank">Github</a>
-                    </Col>
                 </Row>
             </Container>
         
