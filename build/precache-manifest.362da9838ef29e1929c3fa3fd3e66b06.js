8fc93229f251fe1c7b7399bd49d6231c32dd168a
Frank Moran <info@francisfrankmoran.com>
2020-04-28 22:52:32 -0400

    singalong

---
+++ build/precache-manifest.362da9838ef29e1929c3fa3fd3e66b06.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e3f6418e34737c0de62aa3021d2603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e801a5672964cd49894",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a040542283e6685063fb",
+    "url": "/static/css/main.ed8273c9.chunk.css"
+  },
+  {
+    "revision": "0e801a5672964cd49894",
+    "url": "/static/js/2.2a1b0f10.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.2a1b0f10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a040542283e6685063fb",
+    "url": "/static/js/main.34847238.chunk.js"
+  },
+  {
+    "revision": "82f0a2128e246a1f8bb2",
+    "url": "/static/js/runtime-main.3d6ea588.js"
+  },
+  {
+    "revision": "9fd01ce90f7356b8fc99605b2887299d",
+    "url": "/static/media/DSCF5491.9fd01ce9.jpg"
+  },
+  {
+    "revision": "03af0abaa6737095b8d71dbd0818f0cf",
+    "url": "/static/media/animation-background.03af0aba.jpg"
+  },
+  {
+    "revision": "e70379362a383c362e53d0df82d6e5e9",
+    "url": "/static/media/bio-pic.e7037936.jpg"
+  },
+  {
+    "revision": "2336609d7972b9119eb3412f63d16cf2",
+    "url": "/static/media/subtle_white_feathers_@2X.2336609d.png"
+  }
+]);
